6163bc93ffcfcdc5ce874b446cf36ec8c2e49940
bdevops99 <bdevops99@gmail.com>
2023-06-04 23:03:01 +0530

    Vpcs -subnets creations

---
+++ vars.tf
@@ -0,0 +1,18 @@
+variable "tags" {}
+variable "subnets" {}
+variable "env" {}
+variable "name" {
+  default = "elasticcache"
+}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "port_no" {
+  default = 6379
+}
+
+
+variable "node_type" {}
+variable "num_node_groups" {}
+variable "replicas_per_node_group" {}
