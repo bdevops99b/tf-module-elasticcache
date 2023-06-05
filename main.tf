fc5fd52622e766709604d23c5b8143d1606718fd
bdevops99 <bdevops99@gmail.com>
2023-06-05 13:58:42 +0530

    application load balancer

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_elasticache_parameter_group" "main" {
   family      = "redis6.x"
   name        = "elasticache-${var.env}-pg"
   description = "${var.name}-${var.env}-pg"
-  tags        = merge(var.tags, { Name = "${var.name}-${var.env}-pg" })
+  tags        = merge(var.tags, { Name = "elasticache-${var.env}-pg" })
 }
 
 resource "aws_elasticache_replication_group" "main" {
